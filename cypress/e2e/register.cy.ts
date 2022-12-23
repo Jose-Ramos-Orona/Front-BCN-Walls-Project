33402a15025522e5cc4aff5ea3ddd81576559c5a
Jose Ramos <ramosoronaj@gmail.com>
2022-12-23 12:45:18 +0100

    Add video on cypress for record e2e test

--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -1,14 +1,14 @@
 describe("Given BCN Walls register page", () => {
   describe("When the user registers in with username 'jose111', email 'jose111@gmail.com' and password '1111'", () => {
     it("Then it should be redirected to '/login'", () => {
-      cy.visit("http://localhost:3000/register");
+      cy.visit("/register");
       cy.findByRole("heading", { name: "Create an account" }).should("exist");
       cy.findByRole("textbox", { name: "Username" }).type("jose111");
       cy.findByRole("textbox", { name: "Email" }).type("jose111@gmail.com");
       cy.findByLabelText("Password").type("1111");
       cy.findByRole("button", { name: "Register" }).click();
 
-      cy.url().should("include", "/register");
+      cy.url().should("include", "/login");
     });
   });
 });
